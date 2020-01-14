c8e11fa0873fec8ffe5e1ab7de9eb0a49d6b94d2
Olivia Letendart <letendartolivia@gmail.com>
2020-01-14 11:19:44 +0100

    Create new event listener + new function toggleActive + rectification function toggleOpen

--- script.js
+++ script.js
@@ -3,7 +3,11 @@
 const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
-  console.log("Hey");
   this.classList.toggle("open");
 }
+function toggleActive() {
+  this.classList.toggle("open-active");
+}
+
 panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
